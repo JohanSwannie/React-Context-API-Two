a69e6ebf5bdbb4d28781eb014b0ea01d2a4d7243
Johan Swan <jswannieman@gmail.com>
2024-01-19 14:01:50 +1300

    Update Card JSX Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ function Card() {
   }
 
   return (
-    <div className="mb-4 h-auto w-96 border-8 p-20 card">
+    <div className="mb-4 h-3/4 w-auto border-8 p-20 card">
       <img
         className="h-full w-full "
         src="https://upload.wikimedia.org/wikipedia/commons/c/c3/2019_Toyota_Corolla_XSE_%28MZEA12L%29_in_Blue_Flame%2C_front_left.jpg"
